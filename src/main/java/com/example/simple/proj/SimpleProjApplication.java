299ae1e6b6cb29015d09ec338ee49d50ec90337d
Lee Si Hyeon <112377618+tlgusdl03@users.noreply.github.com>
2024-04-28 12:04:44 +0900

    feat : 초기 설정 완료

---
+++ src/main/java/com/example/simple/proj/SimpleProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.simple.proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleProjApplication.class, args);
+	}
+
+}
