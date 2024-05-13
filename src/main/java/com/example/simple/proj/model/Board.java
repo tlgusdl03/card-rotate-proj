dad2d394f34664e8158c31d63fba929473cdd178
tlgusdl03 <tlgusdl03@gmail.com>
2024-05-13 11:02:14 +0900

    feat: 모델 정의

--- src/main/java/com/example/simple/proj/model/Board.java
+++ src/main/java/com/example/simple/proj/model/Board.java
@@ -8,4 +8,52 @@ public class Board {
     User user;
     String title;
     String content;
+
+    public Board(Long id, Date createdTime, User user, String title, String content) {
+        this.id = id;
+        this.createdTime = createdTime;
+        this.user = user;
+        this.title = title;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(Date createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
--- src/main/java/com/example/simple/proj/model/Comment.java
+++ src/main/java/com/example/simple/proj/model/Comment.java
@@ -7,4 +7,43 @@ public class Comment {
     private String content;
     private String author;
     private Date creationDate;
+
+    public Comment(int id, String content, String author, Date creationDate) {
+        this.id = id;
+        this.content = content;
+        this.author = author;
+        this.creationDate = creationDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
 }
